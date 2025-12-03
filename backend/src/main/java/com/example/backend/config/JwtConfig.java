509edb5ba186906d188b4ca2a9b23dede433434c
Thanh Tan <thanhtan8900@gmail.com>
2025-12-03 20:19:32 +0700

    Backend - Cau hinh module Config va Security

--- backend/src/main/java/com/example/backend/config/JwtConfig.java
+++ backend/src/main/java/com/example/backend/config/JwtConfig.java
@@ -1,4 +1,16 @@
 package com.example.backend.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
 public class JwtConfig {
+    private String secret;
+    private long accessTokenExpirationMs = 3600000; // 1 hour
+    private long refreshTokenExpirationMs = 86400000; // 24 hours
 }
